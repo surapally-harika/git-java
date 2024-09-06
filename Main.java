ff72c21764c9b335e3436cc0e7ac6412784a1e6e
surapally-harika <harikaharika6578@gmail.com>
2024-09-06 20:18:56 +0530

    i

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.Employee;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp=new Employee();
+		System.out.print(emp);
+		emp.empinfo();
+		emp.salary();
+	}
+
+}
