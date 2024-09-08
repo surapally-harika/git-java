00adb485d057bccb0c56fb545111cbb58db32038
surapally-harika <harikaharika6578@gmail.com>
2024-09-08 09:05:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.Employee;
 
 public class Main {
 	public static void main(String[] args) {
-		Employee emp=new Employee();
+		Employee emp=new Employee("harika",001,"female",50000,21);
 		System.out.print(emp);
 		emp.empinfo();
 		emp.salary();
