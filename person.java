0283bddaca88a6c5e8d7077e180067e7326a6501
surapally-harika <harikaharika6578@gmail.com>
2024-09-11 07:04:32 +0530

    Create person.java

---
+++ person.java
@@ -0,0 +1,49 @@
+package com.Account;
+
+public class person {
+	
+	public String name;
+	public int age;
+	public String gender;
+	
+	
+	public person(String name, int age, String gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	
+}
