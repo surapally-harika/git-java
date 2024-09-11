a0185c87261748ff3461dbdd1b826af92fe26fb1
surapally-harika <harikaharika6578@gmail.com>
2024-09-11 07:03:38 +0530

    Create account.java

---
+++ account.java
@@ -0,0 +1,42 @@
+package com.Account;
+
+public class account {
+	public int accountid;
+	public int balance;
+	public String accountType;
+	
+	public account(int accountid, int balance, String accountType) {
+		super();
+		this.accountid = accountid;
+		this.balance = balance;
+		this.accountType = accountType;
+	}
+
+	public int getAccountid() {
+		return accountid;
+	}
+
+	public void setAccountid(int accountid) {
+		this.accountid = accountid;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	
+	 
+	
+
+}
