e597ac68c895c2cbc34c735aa6c42e3bc062d7e6
harika <harika.com>
2024-09-12 19:48:41 +0530

    project is created

---
+++ Statemens/src/com/Statement/even.java
@@ -0,0 +1,14 @@
+package com.Statement;
+
+public class even {
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
---
+++ Statemens/src/com/Statement/multiple.java
@@ -0,0 +1,15 @@
+package com.Statement;
+
+public class multiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=21;
+		for(int i=1;i<=21;i++) {
+		if(a%i==0) {
+			System.out.println(i);
+		}
+		}
+
+	}	
+}
