7c5fa437d7628fe96dab57d8ca35b31863bf3184
harika <harika.com>
2024-09-27 10:21:27 +0530

    day-24 commit

--- DSA/src/com/DSA/vowels.java
+++ DSA/src/com/DSA/vowels.java
@@ -8,7 +8,8 @@ public class vowels {
 		String str1="";
 		for(int i=0;i<str.length();i++) {
 			char ch=str.charAt(i);
-			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u') {
+			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'
+					||str.charAt(i)=='A'||str.charAt(i)=='E'||str.charAt(i)=='I'||str.charAt(i)=='O'||str.charAt(i)=='U') {
 				vowels+=ch;
 				 
 			}else {
