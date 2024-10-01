25bf4a033407fc2839b30dae9d5695f2a46c25e7
harika <harika.com>
2024-10-01 09:27:29 +0530

    day-30 sorting

---
+++ DSA/src/com/DSA/sorting.java
@@ -0,0 +1,24 @@
+package com.DSA;
+
+public class sorting {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {8,3,6,2,9,0};
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
