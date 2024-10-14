4d795f523575573a01104f6768b673afd7ec6332
harika <harika.com>
2024-10-14 11:37:23 +0530

    day40 Exceptions

---
+++ ClassAssignments/src/com/Exception/Main.java
@@ -0,0 +1,24 @@
+package com.Exception;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int[] arr = {1,2,3,4};
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the num:");
+		int a = sc.nextInt();
+		try {
+		arr[5] = a;
+		}
+		catch(Exception e) {
+			System.out.println("ArrayOutOfBoundsException");
+		}
+		
+	}
+
+}
