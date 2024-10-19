a0c0f6bf3ae77a009418075bb9272c88a6e6eece
surapally-harika <harikaharika6578@gmail.com>
2024-10-19 08:57:05 +0530

    Arrange elements in odd and even

--- DSA/src/com/DSA/ArrangeElements.java
+++ DSA/src/com/DSA/ArrangeElements.java
@@ -4,13 +4,13 @@ public class ArrangeElements {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {4,3,5,7,2,9,3};
+		int[] arr = {2,3,5,7,4,9,3};
 		for(int i=0;i<arr.length-1;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[i]%2 == 0 && arr[j]%2 != 0 ) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
+			for(int j=0;j<arr.length;j++) {
+				if(arr[j]%2 == 0 && arr[j+1]%2 != 0 ) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
 		}
