69be40e1d4df52b7dac897c1c53e71c07e23a2a5
harika <harika.com>
2024-11-21 13:28:44 +0530

    set matrix zeros

---
+++ DSA/src/com/DSA/setMatrix.java
@@ -0,0 +1,43 @@
+package com.DSA;
+
+public class setMatrix {
+
+	public static void main(String[] args) {
+
+		int[][] matrix = {  {1, 1, 1},
+							{1, 0, 1},
+							{1, 1, 1} };
+
+	        int rows = matrix.length;
+	        int cols = matrix[0].length;
+	        
+	        boolean[] row = new boolean[rows];
+	        boolean[] col = new boolean[cols];
+	        
+	        for (int i = 0; i < rows; i++) {
+	            for (int j = 0; j < cols; j++) {
+	                if (matrix[i][j] == 0) {
+	                    row[i] = true;
+	                    col[j] = true;
+	                }
+	            }
+	        }
+
+	        for (int i = 0; i < rows; i++) {
+	            for (int j = 0; j < cols; j++) {
+	                if (row[i] || col[j]) {
+	                    matrix[i][j] = 0;
+	                }
+	            }
+	        }
+
+	        for (int i = 0; i < rows; i++) {
+	            for (int j = 0; j < cols; j++) {
+	                System.out.print(matrix[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+
+	}
+
+}
